a8f039717546bf73bdde34791fc4133d83ea90b4
chris <mr.chrisnash@gmail.com>
2024-01-29 16:55:03 -0800

    5

--- dojo-helpdesk/app/components/Navbar.jsx
+++ dojo-helpdesk/app/components/Navbar.jsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Logo from "./dojo-logo.png";
 
 export default function NavBar() {
   return (
     <nav>
+      <Image 
+        src={Logo} 
+        width={200} 
+        alt={'Dojo Helpdesk Logo'}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>Dojo Helpdesk</h1>
       <Link href={"/"}>Dashboard</Link>
       <Link href={"/tickets"}>Tickets</Link>
--- dojo-helpdesk/app/layout.jsx
+++ dojo-helpdesk/app/layout.jsx
@@ -1,10 +1,10 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 
 // components
 import NavBar from './components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <NavBar />
-      <body className={inter.className}>{children}</body>
+      <body className={rubik.className}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
