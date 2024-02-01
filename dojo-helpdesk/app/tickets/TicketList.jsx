45ca65337e5dba67f662dbbd3e9835ce8a88cfce
chris <mr.chrisnash@gmail.com>
2024-01-31 17:02:17 -0800

    8

---
+++ dojo-helpdesk/app/tickets/TicketList.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+async function getTickets() {
+
+  // nextjs will automatically cache the response unless the revalidate option is set
+  // here we have set it to 0, so the cache will be invalidated on every request
+  // meaning the data will be fetched from the server on every request
+
+  const response = await fetch('http://localhost:4000/tickets', {
+    next: {
+      revalidate: 0,  // opt out of using cache
+    }
+  });
+  const data = await response.json();
+  
+  return data;
+}
+
+
+export default async function TicketList() {
+  const tickets = await getTickets();
+
+  return (
+    <>
+      {tickets.map(ticket => (
+        <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          
+          </Link>
+        </div>
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets, yay!</p>
+      )}
+    </>
+  )
+}
---
+++ dojo-helpdesk/app/tickets/[id]/page.jsx
@@ -0,0 +1,52 @@
+import { notFound } from "next/navigation";
+
+// true (default): Dynamic segments not included in generateStaticParams are generated on demand
+// false: Dynamic segments not included in generateStaticParams will return a 404
+export const dynamicParams = true;
+
+// by using generateStaticParams, we can statically generate routes at build time
+// instead of on-demand at runtime, this can greatly improve performance and SEO
+export async function generateStaticParams() {
+  const res = await fetch('http://localhost:4000/tickets');
+  const tickets = await res.json();
+  return tickets.map(ticket => ({
+    params: {
+      id: ticket.id
+    }
+  }));
+}
+
+async function getTicket(id) {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: {
+      revalidate: 60
+    }
+  })
+
+  if (!res.ok) {
+    // notFound will cause the page to return a 404 status code
+    notFound();
+  }
+
+  return res.json()
+}
+
+export default async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id);
+
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+        <div className="card">
+          <h3>{ticket.title}</h3>
+          <small>Created by {ticket.user_email}</small>
+          <p>{ticket.body}</p>
+          <div className={`pill ${ticket.priority}`}>
+            {ticket.priority} priority
+          </div>
+        </div>
+        </nav>
+    </main>
+  )
+}
