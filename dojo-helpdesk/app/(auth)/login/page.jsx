fa573e348b203bfd0c4eb959c18d13e18553d47e
chris <mr.chrisnash@gmail.com>
2024-02-12 16:41:24 -0800

    23

--- dojo-helpdesk/app/(auth)/login/page.jsx
+++ dojo-helpdesk/app/(auth)/login/page.jsx
@@ -1,5 +1,20 @@
+"use client"
+
+import AuthForm from "../AuthForm"
+
 export default function Login() {
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+
+    console.log('user login', email, password)
+  }
+
   return (
-    <div>Login</div>
+    <main>
+      <h2 className="text-center">Login</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
+    </main>
   )
 }
--- dojo-helpdesk/app/(auth)/signup/page.jsx
+++ dojo-helpdesk/app/(auth)/signup/page.jsx
@@ -1,5 +1,20 @@
+"use client"
+
+import AuthForm from "../AuthForm"
+
 export default function Signup() {
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+
+    console.log('user signup', email, password)
+  }
+
   return (
-    <div>Signup</div>
+    <main>
+      <h2 className="text-center">Sign up</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
+    </main>
   )
 }
